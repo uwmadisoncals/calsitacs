a86202e5c5bb914b0a1c34c796f5421dff7fadf9
Daniel Dropik <ddropik@wisc.edu>
2015-02-02 12:29:46 -0600

    service pop out is position fixed, and added if structure to ensure that positions are not changed upon resize if  the pop-out is open. Other function added, shrinkBodyHeight, but that may no longer be needed.

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -786,15 +786,28 @@ $(".service-pop-out").velocity({
 });*/
 
 	var documentHeight = $(document).height();
+	var documentHeight2 = documentHeight + 116.781;
 	var windowHeight = $(window).height();
-	console.log(documentHeight);
+	console.log("windowHeight: "+windowHeight);
 
+	function shrinkBodyHeight(){
+		var windowHeight = $(window).height();
+
+		if($(".page-home").length){
+			//alert("on page-home");
+			$("body, html").css({"max-height":"500px","overflow":"hidden"});
+		}
+	}
+	//shrinkBodyHeight();
 
 	//Initialize position.left and position.right on page load
 	var position = $("#services-searchfield").position();
 	var posLeft = "left";
+	var posTop = "top"
 	var opts={};
 	opts[posLeft]=position.left;
+	opts[posTop]=position.top;
+
 	//console.log("position-left: " + position.left + " position-top: " + position.top);
 	$( ".service-pop-out" ).css({top:position.top,left:position.left});
 
@@ -802,14 +815,21 @@ $(".service-pop-out").velocity({
 	$(window).resize(function(){
 		var documentHeight = $(window).height();
 
-		var position = $("#services-searchfield").position();
-
-		//var posLeft = "left";
-		//var opts={};
-		//opts[posLeft]=position.left;
+		position = $("#services-searchfield").position();
+		posLeft = "left";
+		posTop = "top"
+		opts={};
+		opts[posLeft]=position.left;
+		opts[posTop]=position.top;
+		
 		//console.log(opts);
-		//$( ".service-pop-out" ).css({top:position.top,left:position.left});
-		$( ".service-pop-out" ).css({left:position.left});
+		if(!$(".service-pop-out").hasClass("open")){
+
+			$( ".service-pop-out" ).css({top:position.top,left:position.left});
+			
+		}
+		
+		//$( ".service-pop-out" ).css({left:position.left});
 		//console.log("position-left: " + position.left + " position-top: " + position.top);
 		console.log("posLeft: " + opts[posLeft] + ", documentHeight: " + documentHeight);
 
@@ -820,6 +840,8 @@ $(".service-pop-out").velocity({
 	$( ".our-services" ).click(function(e) {
 
 		e.preventDefault();
+
+
 
 	  	$(".service-pop-out").velocity({
 
@@ -839,9 +861,12 @@ $(".service-pop-out").velocity({
 	$("#services-searchfield").focus(function(e) {
 
 		e.preventDefault();
+
+	
+
 		$(".service-pop-out").velocity({
 
-		properties:{ width: "100%", height: documentHeight , left: "0px", top: "0px", opacity:"1"/*,translateX:"-200px"*/},
+		properties:{ width: "100%", height: windowHeight , left: "0px", top: "0px", opacity:"1"/*,translateX:"-200px"*/},
 					 		
 		options:{ duration: "800", easing:"easeInOutCubic", display:"block"}
 
@@ -860,6 +885,10 @@ $(".service-pop-out").velocity({
 
 		});*/
 
+		$(".service-pop-out").addClass("open");
+
+
+
 	});
 
 
@@ -870,12 +899,12 @@ $(".service-pop-out").velocity({
 
 	  	$(".service-pop-out").velocity({
 
-	 		properties:{ width: "100", height: "100" , opacity:"0"},
+	 		properties:{ width: "100", height: "100" ,left:opts[posLeft], top:opts[posTop], opacity:"0"},
 	 		
 	  		options:{ duration: "800", easing:"easeInOutCubic", display:"none"}
 		});
 
-		
+		$(".service-pop-out").removeClass("open");
 	});
 
 
--- js/CALSboilerplate_underscores_custom.js
+++ js/CALSboilerplate_underscores_custom.js
@@ -20,15 +20,28 @@ $(".service-pop-out").velocity({
 });*/
 
 	var documentHeight = $(document).height();
+	var documentHeight2 = documentHeight + 116.781;
 	var windowHeight = $(window).height();
-	console.log(documentHeight);
+	console.log("windowHeight: "+windowHeight);
 
+	function shrinkBodyHeight(){
+		var windowHeight = $(window).height();
+
+		if($(".page-home").length){
+			//alert("on page-home");
+			$("body, html").css({"max-height":"500px","overflow":"hidden"});
+		}
+	}
+	//shrinkBodyHeight();
 
 	//Initialize position.left and position.right on page load
 	var position = $("#services-searchfield").position();
 	var posLeft = "left";
+	var posTop = "top"
 	var opts={};
 	opts[posLeft]=position.left;
+	opts[posTop]=position.top;
+
 	//console.log("position-left: " + position.left + " position-top: " + position.top);
 	$( ".service-pop-out" ).css({top:position.top,left:position.left});
 
@@ -36,14 +49,21 @@ $(".service-pop-out").velocity({
 	$(window).resize(function(){
 		var documentHeight = $(window).height();
 
-		var position = $("#services-searchfield").position();
-
-		//var posLeft = "left";
-		//var opts={};
-		//opts[posLeft]=position.left;
+		position = $("#services-searchfield").position();
+		posLeft = "left";
+		posTop = "top"
+		opts={};
+		opts[posLeft]=position.left;
+		opts[posTop]=position.top;
+		
 		//console.log(opts);
-		//$( ".service-pop-out" ).css({top:position.top,left:position.left});
-		$( ".service-pop-out" ).css({left:position.left});
+		if(!$(".service-pop-out").hasClass("open")){
+
+			$( ".service-pop-out" ).css({top:position.top,left:position.left});
+			
+		}
+		
+		//$( ".service-pop-out" ).css({left:position.left});
 		//console.log("position-left: " + position.left + " position-top: " + position.top);
 		console.log("posLeft: " + opts[posLeft] + ", documentHeight: " + documentHeight);
 
@@ -54,6 +74,8 @@ $(".service-pop-out").velocity({
 	$( ".our-services" ).click(function(e) {
 
 		e.preventDefault();
+
+
 
 	  	$(".service-pop-out").velocity({
 
@@ -73,9 +95,12 @@ $(".service-pop-out").velocity({
 	$("#services-searchfield").focus(function(e) {
 
 		e.preventDefault();
+
+	
+
 		$(".service-pop-out").velocity({
 
-		properties:{ width: "100%", height: documentHeight , left: "0px", top: "0px", opacity:"1"/*,translateX:"-200px"*/},
+		properties:{ width: "100%", height: windowHeight , left: "0px", top: "0px", opacity:"1"/*,translateX:"-200px"*/},
 					 		
 		options:{ duration: "800", easing:"easeInOutCubic", display:"block"}
 
@@ -94,6 +119,10 @@ $(".service-pop-out").velocity({
 
 		});*/
 
+		$(".service-pop-out").addClass("open");
+
+
+
 	});
 
 
@@ -104,12 +133,12 @@ $(".service-pop-out").velocity({
 
 	  	$(".service-pop-out").velocity({
 
-	 		properties:{ width: "100", height: "100" , opacity:"0"},
+	 		properties:{ width: "100", height: "100" ,left:opts[posLeft], top:opts[posTop], opacity:"0"},
 	 		
 	  		options:{ duration: "800", easing:"easeInOutCubic", display:"none"}
 		});
 
-		
+		$(".service-pop-out").removeClass("open");
 	});
 
 
