d7ce75a9b33ae90fb982c81d93979fc49b0a52a9
Daniel Dropik <ddropik@wisc.edu>
2015-04-13 12:45:26 -0500

    Inserting markup and css to display codrops inspired input field

--- header.php
+++ header.php
@@ -61,11 +61,19 @@ if ( is_front_page() ) { ?>
 		<h2>CALS IT/ACS</h2>
 		<h2 class="subHeading">Here for you.</h2>
 		
-		<div class="services-searchbox">
-				<input type="text" id="services-searchfield" value="" placeholder="Search">
-				<a href="#" class="our-services button large blue">Our Services</a>
+		<!--<div class="services-searchbox">
+				
+				<input  type="text" id="services-searchfield" value="" placeholder="Search">
+				<a href="#" class="our-services button large blue">Our Services</a> 
+				
 		</div><!-- END .searchbox-->
 	</div>
+		<span class="input input--minoru">
+						<input class="input__field input__field--yoko" type="text" id="input-16" />
+						<label class="input__label input__label--yoko" for="input-16">
+							<span class="input__label-content input__label-content--yoko">Street</span>
+						</label>
+					</span>
 
 <?php } ?>
 	<div class="height-div"></div>
