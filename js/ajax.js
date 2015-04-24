f3385434494879ac4b3452a1590157cd09eb1dc9
Daniel Dropik <ddropik@wisc.edu>
2015-04-24 14:17:05 -0500

    removing git confict markers and restoring js/ajax.js to the most correct and updated state

--- js/ajax.js
+++ js/ajax.js
@@ -1,20 +1,6 @@
 jQuery(document).ready(function($) {
-<<<<<<< HEAD
-<<<<<<< HEAD
- 
-    var ajax_url = ajax_params.ajax_url; // so we access our ajax_url through the ajax_params object
-    var data = {
-        'action': 'my_action',
-        'whatever': 1234
-    };
- 
-    $.post(ajax_url, data, function(response) {
-        //alert(response.whatever);
-    });
-});
-=======
-=======
->>>>>>> 4da3996e1e5b97a0aa245a06ee63c7556e3c9225
+
+	var appended = false;
 
 	if($('body').is('.page-home')){
 
@@ -25,37 +11,52 @@ jQuery(document).ready(function($) {
 	    dataType:"json",
 	    whatever: 1234
 	  };
+
+
 	 
 	  // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
 	  
 	  // This is the ajax request to the server
 	  $.post(MyAjax.ajaxurl, data, function(response) {
 
-<<<<<<< HEAD
-	    console.log(response); //this is the ajax success callback function, it manipulates the RESPONSE from the server
-
-	    console.log( response[1].post_title );
-
-	  });
-
-	}
-
-}); //END jQuery.ready
->>>>>>> 1fd08164ba29562a1aa87a007b22e27ee8cd5103
-=======
 	  		var ajaxResponseLength = response.length;
+	  		var noOfMoreResults = ajaxResponseLength-10;
 
 	    	//console.log(response); //this is the ajax success callback function, it manipulates the RESPONSE from the server	
 
-			for( i=0; i < ajaxResponseLength; i++ ) {
+		
+				
+			$("#input-16").focus(function(){
 
-				$(".searchResults ul").append("<li class='singleSearchResult' ><a class='linkSearchResult' href='" + response[i].post_permalink + "'>" + response[i].post_title + "</a></li>");
+				if (!appended){
 
-			}
+					for( i=0; i < 10; i++ ) {
+
+						$(".searchResults ul").append("<li class=\"singleSearchResult\"><a class=\"linkSearchResult\" href='" + response[i].post_permalink + "'>" + response[i].post_title + "</a></li>");
+
+
+						appended = true;
+
+					
+					}
+
+					$(".searchResults ul").append("<li class=\"moreCommon\"><a class=\"linkMoreCommon\" href='#'>" + noOfMoreResults + " more...</a></li>");
+
+				}
+				
+				
+
+				
+			});
+
+	
+
+
+
+			
 
 	  }); //END $.post
 
 	}//END ispage home
 
 }); //END jQuery.ready
->>>>>>> 4da3996e1e5b97a0aa245a06ee63c7556e3c9225
