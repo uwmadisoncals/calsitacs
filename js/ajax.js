1e0c5710d604e40acbf6fbec3126e59e192832d2
Daniel Dropik <ddropik@wisc.edu>
2015-04-24 12:44:47 -0500

    Fix jagged text originating from jQuery animation by applying css hacks and changing the velocity animation from slideLeftIn to fadeIn. Adding styles to search result <li> and <a> tags. Nesting jquery append functions within on-focus and within if statement to ensure it executes once,

--- js/ajax.js
+++ js/ajax.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function($) {
 
+	var appended = false;
+
 	if($('body').is('.page-home')){
 
 
@@ -9,6 +11,8 @@ jQuery(document).ready(function($) {
 	    dataType:"json",
 	    whatever: 1234
 	  };
+
+
 	 
 	  // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
 	  
@@ -16,14 +20,40 @@ jQuery(document).ready(function($) {
 	  $.post(MyAjax.ajaxurl, data, function(response) {
 
 	  		var ajaxResponseLength = response.length;
+	  		var noOfMoreResults = ajaxResponseLength-10;
 
-	    	console.log(response); //this is the ajax success callback function, it manipulates the RESPONSE from the server	
+	    	//console.log(response); //this is the ajax success callback function, it manipulates the RESPONSE from the server	
 
-			for( i=0; i < ajaxResponseLength; i++ ) {
+		
+				
+			$("#input-16").focus(function(){
 
-				$(".searchResults ul").append("<li><a href='" + response[i].post_permalink + "'>" + response[i].post_title + "</a></li>");
+				if (!appended){
 
-			}
+					for( i=0; i < 10; i++ ) {
+
+						$(".searchResults ul").append("<li class=\"singleSearchResult\"><a class=\"linkSearchResult\" href='" + response[i].post_permalink + "'>" + response[i].post_title + "</a></li>");
+
+
+						appended = true;
+
+					
+					}
+
+					$(".searchResults ul").append("<li class=\"moreCommon\"><a class=\"linkMoreCommon\" href='#'>" + noOfMoreResults + " more...</a></li>");
+
+				}
+				
+				
+
+				
+			});
+
+	
+
+
+
+			
 
 	  }); //END $.post
 
