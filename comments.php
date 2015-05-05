29e5fec2bede62dd2b8c3174ad7f930750e99efb
Daniel Dropik <ddropik@wisc.edu>
2015-05-05 14:14:18 -0500

    Style edits to single-post; comments re-styling
    Adding new sass files and css rule sets to define a more cohesive and appealing design for default comment styles.
    
    Adding more margin & padding  rules for the single-post.php

--- comments.php
+++ comments.php
@@ -28,6 +28,7 @@ if ( post_password_required() ) {
 				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'calsboilerplate_underscores' ),
 					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
 			?>
+
 		</h2>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
