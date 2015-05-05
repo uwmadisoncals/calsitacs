01368b14fe1ef08b1eee1683fe55211cb40a5624
Daniel Dropik <ddropik@wisc.edu>
2015-05-05 15:53:41 -0500

    positioning links in comments

--- comments.php
+++ comments.php
@@ -28,7 +28,6 @@ if ( post_password_required() ) {
 				printf( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'calsboilerplate_underscores' ),
 					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
 			?>
-
 		</h2>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
