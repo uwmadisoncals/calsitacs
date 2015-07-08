0ced3c42cd75e8fed7e8985b303522fee0a05ca8
Daniel Dropik <ddropik@wisc.edu>
2015-07-08 08:09:35 -0500

    Add Sass to archive.scss

--- 404.php
+++ 404.php
@@ -9,8 +9,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
+  			<section class="error-404 not-found">
 				<header class="page-header">
 					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'calsboilerplate_underscores' ); ?></h1>
 				</header><!-- .page-header -->
